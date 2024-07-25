6f15f0708b0e3e9071a8f78caf87f08f7db74064
Mike Doherty <145161084+mikedoherty1@users.noreply.github.com>
2024-07-25 11:46:55 -0400

    New Setup for anible owner

---
+++ step1.sh
@@ -0,0 +1,49 @@
+#Basic os commands to configure pre-ansible install
+#Allow root
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+echo ""
+echo ""
+echo ""
+echo ""
+systemctl restart sshd
+echo ""
+echo ""
+echo ""
+echo ""
+#Get ip and hostname to read into variable
+hn=`hostname`
+ips=`ifconfig | grep 192 | awk '{ print $2}'`
+echo "IP and hostname being added to /etc/hosts $hnip : $hn"
+echo "$ips     $hn" >> /etc/hosts
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Enter you Mac IP address "
+read ipsm
+echo ""
+echo ""
+echo ""
+echo "Enter your Mac hostname > "
+read hnm
+echo "$ipsm     $hnm" >> /etc/hosts
+echo ""
+echo ""
+echo ""
+#Generate SSH Key
+ssh-keygen -t rsa
+echo ""
+echo ""
+echo ""
+ssh-keyscan -H $hn >> ~/.ssh/known_hosts
+echo ""
+echo ""
+echo ""
+cat /root/.ssh/id_rsa.pub > /root/.ssh/authorized_keys
+#add ansible 
+dnf install ansible-core -y
+echo ""
+echo ""
+echo ""
+ansible-galaxy collection install ansible.posix
